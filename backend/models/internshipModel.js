51bdaa3d8dc0bbddc82f57a099b36112630facad
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-06-14 21:07:28 -0300

    Model para os estagios

---
+++ backend/models/internshipModel.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+  professionalProfile: {
+    type: String,
+    required: [ true, "Perfil do profissional deve ser informado" ]
+  },
+  jobProfile: {
+    type: String,
+    required: [ true, "Perfil da vaga deve ser informado" ]
+  },
+  benefits: {
+    type: String,
+    required: false
+  },
+  workModality: {
+    type: String,
+    required: [ true, "Estilo de trabalho deve ser informado" ],
+    enum: {
+      values: [ "presencial", "remoto", "híbrido"],
+      message: "Estilo de trabalho deve ser presencial, remoto ou híbrido"
+    }
+  },
+  // activity: {},
+  weeklyWorkload: {
+    type: Number,
+    required: [ true, "Carga horária semanal deve ser informada" ]
+  },
+  applicationProcedures: {
+    type: String,
+    required: [ true, "Procedimentos de candidatura devem ser informados" ]
+  },
+  available: {
+    type: Boolean,
+    required: [ true, "Disponibilidade deve ser informada" ],
+    default: true
+  },
+  coordinatorEmail: {
+    type: mongoose.Types.ObjectId,
+    ref: "Coordinator",
+    required: [ true, "Email do coordenador deve ser informado" ]
+  },
+  enterpriseCnpj: {
+    type: mongoose.Types.ObjectId,
+    ref: "Enterprise",
+    required: [ true, "CNPJ da empresa deve ser informado" ]
+  },
+}, { timestamps: true });
+
+const Internship = mongoose.model("Internship", schema);
+
+export default Internship;
