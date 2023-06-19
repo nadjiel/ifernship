a073302be400a006013e16ca35a895a4aa0e84c6
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-06-19 13:54:45 -0300

    Adaptando o codigo para o mongoose

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,8 +1,10 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
 // const connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/?retryWrites=true&w=majority`;
 const connectionString = "mongodb://127.0.0.1:27017";
 
-const client = new MongoClient(connectionString);
+async function connect() {
+  await mongoose.connect(connectionString);
+}
 
-export default client;
+export default connect;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import connect from "./database/index.js";
 import internshipRouter from "./routes/internshipRouter.js";
 import enterpriseRouter from "./routes/enterpriseRouter.js";
 dotenv.config();
@@ -12,6 +13,15 @@ app.use(express.json());
 app.use("/internship", internshipRouter);
 app.use("/enterprise", enterpriseRouter);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+async function start() {
+  try {
+    await connect();
+
+    console.log("Successfully connected to DB");
+    console.log(`App listening on port ${PORT}`);
+  } catch(err) {
+    console.log(`Not connected to DB. Error: ${err}`);
+  }
+}
+
+app.listen(PORT, start);
