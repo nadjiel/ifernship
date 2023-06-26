cd6e3b85f28623e3d5389888e49a7febc71079a0
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-06-26 20:54:51 -0300

    Adicionando constantes para estilizacao

---
+++ frontend/global/theme.js
@@ -0,0 +1,35 @@
+export default theme = {
+  colors: {
+    primary: "#00ADB4",
+    faded: "#C1C1C2",
+    secondary: "#62C751",
+    thirdiary: "#F2F5F9",
+    cancel: "#FB1212"
+  },
+  fontColors: {
+    primary: "#030A1D",
+    faded: "#717171",
+    secondary: "#62C751",
+    thirdiary: "#F2F5F9"
+  },
+  fonts: {
+    outfitLight: "outfit-light",
+    outfitMedium: "outfit-medium",
+    outfitBold: "outfit-bold",
+    poppinsLight: "poppins-light",
+    poppinsRegular: "poppins-regular",
+    poppinsMedium: "poppins-medium",
+    poppinsSemibold: "poppins-semibold",
+    poppinsBold: "poppins-bold",
+    assistantLight: "assistant-light",
+    assistantRegular: "assistant-regular",
+    assistantSemibold: "assistant-semibold"
+  },
+  fontSizes: {
+    xs: "30px",
+    s: "20px",
+    m: "16px",
+    l: "14px",
+    xl: "12px",
+  }
+};
