8f94245824715b05b7f0c58065f3c886bf957caa
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-06-26 14:03:07 -0300

    Importando fontes externas

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import * as Font from "expo-font";
+
+import Title from "./components/Title/index.jsx";
 
 export default function App() {
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      "assistant-light": require("./assets/fonts/Assistant-Light.ttf"),
+      "assistant-regular": require("./assets/fonts/Assistant-Regular.ttf"),
+      "assistant-semibold": require("./assets/fonts/Assistant-SemiBold.ttf"),
+      "outfit-light": require("./assets/fonts/Outfit-Light.ttf"),
+      "outfit-medium": require("./assets/fonts/Outfit-Medium.ttf"),
+      "outfit-bold": require("./assets/fonts/Outfit-Bold.ttf"),
+      "poppins-light": require("./assets/fonts/Poppins-Light.ttf"),
+      "poppins-regular": require("./assets/fonts/Poppins-Regular.ttf"),
+      "poppins-medium": require("./assets/fonts/Poppins-Medium.ttf"),
+      "poppins-semibold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+      "poppins-bold": require("./assets/fonts/Poppins-Bold.ttf")
+    });
+  };
+
   return (
     <View style={styles.container}>
+      <Title>Hello, World!</Title>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
