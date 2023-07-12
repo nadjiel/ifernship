ffaaaa73770ea1505ecc49c8690e2748adb374a2
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2023-07-12 20:29:59 -0300

    Criando Componente de StepCard e Página de ProcessSteps

---
+++ frontend/components/StepsCard/index.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Container } from "./style.js";
+
+export default function StepsCard({
+}) {
+	return (
+		<Container>
+			<View>
+			</View>
+		</Container>
+	);
+}
