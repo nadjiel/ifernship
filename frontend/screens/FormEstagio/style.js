a0bf3cbaf79e5480faa71f127c296fceb524ae0e
marianemoreira777 <mariane.moreira@academico.ifpb.edu.br>
2023-07-12 20:17:21 -0300

    add botao da tela estagio e importando algumas pendencias

--- frontend/screens/FormEstagio/style.js
+++ frontend/screens/FormEstagio/style.js
@@ -11,4 +11,11 @@ export default styles = StyleSheet.create({
 		marginBottom: 10,
 		marginHorizontal: 25,
 	},
+    
+    buttons: {
+		marginTop: 30,
+		width: '100%',
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
 });
