6867c17fea24440f966fe72579dd92a5a13354fa
marianemoreira777 <mariane.moreira@academico.ifpb.edu.br>
2023-07-12 18:01:58 -0300

    Usando componente inputext na tela da entidade Empresa

--- frontend/screens/FormEmpresa/index.jsx
+++ frontend/screens/FormEmpresa/index.jsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import Title from '../../components/Title/index.jsx';
+import InputText from '../../components/Input/index.jsx';
+
 import styles from './style.js';
 
 export default function EmpresaForm() {
@@ -9,6 +11,17 @@ export default function EmpresaForm() {
 			<View style={styles.main}>
 				<Title>Adicionar dados da Empresa</Title>
 			</View>
+
+            <View>
+				<InputText placeholderText={'Nome'} />
+				<InputText placeholderText={'Sobre a empresa'} />
+				<InputText placeholderText={'CNPJ'} />
+				<InputText placeholderText={'Email'} />
+				<InputText placeholderText={'Cidade'} />
+				<InputText placeholderText={'Latitude'} />
+				<InputText placeholderText={'Longitude'} />
+			</View>
+
             <StatusBar style="auto" />
 		</View>
 	);
