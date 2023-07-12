7abbdad86d8f5760b17eec5efc8ef2b2a8d094a1
marianemoreira777 <mariane.moreira@academico.ifpb.edu.br>
2023-07-12 20:12:13 -0300

    add codigo do arquivo style.js

---
+++ frontend/screens/FormEstagio/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+	},
+
+	main: {
+		gap: 25,
+		marginTop: 80,
+		marginBottom: 10,
+		marginHorizontal: 25,
+	},
+});
