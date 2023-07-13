fdf007c9f222cc0d79e968ae77d328ce9d39030d
Lucas Marques Garrido <lucas-mgarrido20@outlook.com>
2023-07-13 00:37:28 -0300

    Adicioanando validação yup

--- frontend/utils/validations.js
+++ frontend/utils/validations.js
@@ -1,15 +1,15 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 export const schemaValidationLogin = yup.object({
 	email: yup
 		.string()
-		.email("email inválido")
-		.required("Por favor, forneça um email!"),
+		.email('email inválido')
+		.required('Por favor, forneça um email!'),
 	password: yup
 		.string()
-		.min(5, "password menor que 5 caracteres")
-		.max(10, "password maior que 10 caracteres")
-		.required("Por favor, forneça uma senha!"),
+		.min(5, 'password menor que 5 caracteres')
+		.max(10, 'password maior que 10 caracteres')
+		.required('Por favor, forneça uma senha!'),
 });
 
 export const schemaValidationRegister = yup.object({
@@ -17,34 +17,36 @@ export const schemaValidationRegister = yup.object({
 	course: yup.string(),
 	email: yup
 		.string()
-		.email("email inválido")
-		.required("Por favor, forneça um email!"),
+		.email('email inválido')
+		.required('Por favor, forneça um email!'),
 	password: yup
 		.string()
-		.min(5, "password menor que 5 caracteres")
-		.max(10, "password maior que 10 caracteres")
-		.required("Por favor, forneça uma senha!"),
-		
-export const schemaValidationInternship = yup.object({
-  profile: yup
-    .string()
-    .required("Por favor, forneça um perfil!"),
-  benefits: yup
-    .string(),
-  type: yup
-    .string()
-    .required("Por favor, forneça um tipo de trabalho!"),
-  course: yup
-    .string()
-    .required("Por favor, forneça um curso!"),
-  position: yup
-    .string()
-    .required("Por favor, forneça um cargo!"),
-  hoursWeek: yup
-    .number()
-    .required("Por favor, forneça uma carga horaria semanal!"),
-  enterprise: yup
-    .string()
-    .required("Por favor, forneça uma empresa!"),
+		.min(5, 'password menor que 5 caracteres')
+		.max(10, 'password maior que 10 caracteres')
+		.required('Por favor, forneça uma senha!'),
 });
+
+export const schemaValidationEnterprise = yup.object({
+	name: yup.string().required('Por favor, forneça um nome!'),
+	about: yup.string(),
+	cnpj: yup.string().required('Por favor, forneça um CNPJ!'),
+	email: yup
+		.string()
+		.email('email inválido')
+		.required('Por favor, forneça um email!'),
+	city: yup.string().required('Por favor, forneça uma cidade!'),
+	latitude: yup.string().required('Por favor, forneça uma senha!'),
+	longitude: yup.string().required('Por favor, forneça uma senha!'),
+});
+
+export const schemaValidationInternship = yup.object({
+	profile: yup.string().required('Por favor, forneça um perfil!'),
+	benefits: yup.string(),
+	type: yup.string().required('Por favor, forneça um tipo de trabalho!'),
+	course: yup.string().required('Por favor, forneça um curso!'),
+	position: yup.string().required('Por favor, forneça um cargo!'),
+	hoursWeek: yup
+		.number()
+		.required('Por favor, forneça uma carga horaria semanal!'),
+	enterprise: yup.string().required('Por favor, forneça uma empresa!'),
 });
