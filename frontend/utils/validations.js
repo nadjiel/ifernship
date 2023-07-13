a13acf47864a71c0561758f4b754b433b141ddcd
marianemoreira777 <mariane.moreira@academico.ifpb.edu.br>
2023-07-13 00:32:50 -0300

    add validação register

--- frontend/utils/validations.js
+++ frontend/utils/validations.js
@@ -24,4 +24,27 @@ export const schemaValidationRegister = yup.object({
 		.min(5, "password menor que 5 caracteres")
 		.max(10, "password maior que 10 caracteres")
 		.required("Por favor, forneça uma senha!"),
+		
+export const schemaValidationInternship = yup.object({
+  profile: yup
+    .string()
+    .required("Por favor, forneça um perfil!"),
+  benefits: yup
+    .string(),
+  type: yup
+    .string()
+    .required("Por favor, forneça um tipo de trabalho!"),
+  course: yup
+    .string()
+    .required("Por favor, forneça um curso!"),
+  position: yup
+    .string()
+    .required("Por favor, forneça um cargo!"),
+  hoursWeek: yup
+    .number()
+    .required("Por favor, forneça uma carga horaria semanal!"),
+  enterprise: yup
+    .string()
+    .required("Por favor, forneça uma empresa!"),
+});
 });
