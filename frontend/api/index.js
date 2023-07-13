eaa76422d9c4e7bc1af0d0da4e454f18dbb39e10
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-07-13 20:17:53 -0300

    Adicionando errors detalhados a api do axios

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -5,4 +5,18 @@ const api = axios.create({
   baseURL: `http://${IP_HOST}:8080/`,
 });
 
+function doNothing(res) {
+  return res;
+}
+
+api.interceptors.response.use(doNothing, (error) => {
+  const { response } = error;
+  if (response) {
+    console.error(response);
+  } else {
+    console.error(error);
+  }
+  throw error;
+});
+
 export default api;
